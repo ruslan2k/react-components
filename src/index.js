c1b5ad6f82b08ddc343cfbe5d97a9d69d1db6637
Ruslan Karachun <free.changer@gmail.com>
2018-09-02 14:15:48 +0300

    Added LoginForm

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class LoginForm extends Component {
+  render () {
+    return (
+      <form>
+        <h1>Login</h1>
+        <input type='text' />
+      </form>
+    )
+  }
+}
