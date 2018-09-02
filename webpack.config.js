bdbe9b0ae1052137d304b62a15bc8494cb6ee21e
Ruslan Karachun <free.changer@gmail.com>
2018-09-02 14:49:33 +0300

    Added babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {loader: 'babel-loader'}
+      }
+    ]
+  }
+}
