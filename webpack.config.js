01e178c1156c9c0b50bb8ed7e7c74413fa625801
Ruslan Karachun <free.changer@gmail.com>
2018-09-03 16:41:50 +0300

    Added build dir to main definition

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,10 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
-        use: {loader: 'babel-loader'}
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader'
+        }
       }
     ]
   }
