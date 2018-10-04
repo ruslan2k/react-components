6c49db899ddf9030c2f470694fa5c006a6bd087b
Ruslan Karachun <free.changer@gmail.com>
2018-10-04 10:06:23 +0300

    Added nested routes & debug info

--- src/test.js
+++ src/test.js
@@ -4,8 +4,28 @@ import { HashRouter as Router, Link, Route } from 'react-router-dom'
 
 import { Model, LoginForm } from './index'
 
-const Users = () => <Model name='users' items={[1, 2, 3]} />
 const Home = () => <h3>Home</h3>
+const UserInfo = ({ match }) => {
+  const userId = match.params.userId
+  return (
+    <div>
+      <h3>User Info</h3>
+      <p>id: {userId}</p>
+    </div>
+  )
+}
+const UsersList = (props) => {
+  console.log(props);
+  return <h3>UsersList</h3>;
+}
+
+const Users = (props) => (
+  <div>
+    <Route exact path='/users' component={UsersList} />
+    <Route path='/users/:userId' component={UserInfo} />
+  </div>
+)
+const UserView = () => <h3>UserView</h3>
 
 const App = () => {
   return (
@@ -13,6 +33,7 @@ const App = () => {
       <div>
         <Link to='/'>[Home]</Link>
         <Link to='/users'>[Users]</Link>
+        <Link to='/users/name'>[User Vasya]</Link>
         <Route exact path='/' component={Home} />
         <Route path='/users' component={Users} />
       </div>
